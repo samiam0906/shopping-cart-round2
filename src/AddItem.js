54cddeebd056c07bba9d788fa0806d24657140b1
Jen Shannon <jennifer.shannon@galvanize.com>
2017-11-22 10:34:00 -0500

    Add Item

--- src/AddItem.js
+++ src/AddItem.js
@@ -4,14 +4,21 @@ import { Row, Input, Button } from 'react-materialize'
 const AddItem = ({ products, itemAdded}) => {
   const itemWasAdded = (e) => {
     e.preventDefault()
-    itemAdded()
+    console.log(e.target)
+    itemAdded({
+      quantity: e.target.quantity.value,
+      product: products.find( (product) => product.id === parseInt(e.target.productId.value)) 
+    })
   }
   return (
     <form className="container" onSubmit={ itemWasAdded }>
     <Row>
-      <Input s={12} label="Choose a Product" type='text' name="productId" >
+      <Input s={12} label="Choose a Product" type='select' name="productId" >
         { products.map(product => <option key={product.id} value={product.id}>{ product.name }</option>)}
       </Input>
+      </Row>
+      <Row>
+        <Input s={6} type='number' label='Quantity' defaultValue='1' name='quantity'></Input>
       </Row>
       <Row>
       <Button>Add Item</Button>
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -5,44 +5,24 @@ import CartItems from './CartItems'
 import AddItem from './AddItem'
 
 
-// const ShoppingCart = ({items, year}) => {
-//   // console.log('CartItems props:', items)
-//   // console.log('ShoppingCart props:', props)
-//   return (
-//     <div>
-//       <CartHeader />
-//       <CartItems items={items}/>
-//       <CartFooter year={year}/>
-//     </div>
-//   )
-// }
-const products = [
-{ id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
-{ id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
-{ id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
-{ id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
-{ id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
-{ id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
-{ id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
-{ id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
-{ id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
-]
 class ShoppingCart extends React.Component {
 
-  constructor(props) {
+  constructor(props){
     super(props)
     this.state = { items: props.items}
   }
 
-  itemWasAdded = (item) => {
-    console.log("This is the item: ", item)
+  handleItemAdded = (item) => {
+    item.id = this.state.items.length
+    this.setState({ items: [item,...this.state.items]})
   }
+
   render(){
     return (
       <div>
         <CartHeader />
-        <AddItem products={products} itemAdded={ this.itemWasAdded } />
-        <CartItems items={this.props.items}/>
+        <AddItem products={this.props.products} itemAdded={ this.handleItemAdded } />
+        <CartItems items={this.state.items}/>
         <CartFooter year={this.props.year}/>
       </div>
     )
